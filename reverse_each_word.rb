42cca65b90abf5d55e422b046b5e99ef159ae715
Haley Someone <hbarlar1@gmail.com>
2018-07-24 17:52:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,9 @@
 def reverse_each_word(string)
   array_string = string.split(" ")
   newArray = []
-  array_string.each do |x|
+  
+  array_string.collect do |x|
   newArray << array_string.pop
 end
-puts newArray
+newArray.join
 end
