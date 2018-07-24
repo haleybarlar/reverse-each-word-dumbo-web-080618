e71197f74a3a347c0910a9baef800d9ff57cdbab
Haley Someone <hbarlar1@gmail.com>
2018-07-24 17:46:31 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,4 @@
-def reverse_each_word(string)
-  array_string = string.split("")
-  newArray = []
-  newArray << array_string.pop
-end
+
 
 def reverse_each_word(string)
   array_string = string.split(" ")
